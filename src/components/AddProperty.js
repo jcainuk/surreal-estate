5da06aa8cb2a70a0acd5dffebe69e7a3aa5cb68a
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-24 15:07:43 +0100

    Refactored the AddProperty component

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
 import "../styles/AddProperty.css";
 
-const initialState = {
-  fields: {
-    title: "",
-  },
-};
-const [fields, setFields] = useState(initialState.fields);
+const AddProperty = () => {
+  const initialState = {
+    fields: {
+      title: "",
+    },
+  };
+  const [fields, setFields] = useState(initialState.fields);
 
-const AddProperty = () => (
-  <div className="AddProperty">
-    <form action="">
-      <button type="submit">Add</button>
-    </form>
-  </div>
-);
+  return (
+    <div className="AddProperty">
+      Add Property:
+      <form>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddProperty;
