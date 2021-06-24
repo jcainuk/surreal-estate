935ec5877b8b24d88c0173e073a76dc370b14b78
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-24 13:33:19 +0100

    Added a new <Route /> component inside the <Switch /> statement

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "../styles/App.css";
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import NavBar from "./NavBar";
+import Properties from "./Properties";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Switch />
+      <Switch>
+        <Route exact path="/" component={Properties} />
+      </Switch>
     </div>
   );
 }
