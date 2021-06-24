29ea45eabc0b3ee3c6884f562834df06a664d3d4
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-24 15:44:38 +0100

    Added a <select> element for cities with some different options

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -32,6 +32,20 @@ const AddProperty = () => {
             onChange={handleFieldChange}
           />
         </label>
+        <label htmlFor="city">
+          City
+          <select
+            id="city"
+            name="city"
+            value={fields.city}
+            onChange={handleFieldChange}
+          >
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+            <option value="Sheffield">Sheffield</option>
+            <option value="Liverpool">Liverpool</option>
+          </select>
+        </label>
         <button type="submit">Add</button>
       </form>
     </div>
