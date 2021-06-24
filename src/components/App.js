8d2e6bf3a28f5291f53a4e296ad3d58234c09d86
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-24 13:30:02 +0100

    Rendered a <Switch /> component inside the <App /> component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/App.css";
+import { Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Switch />
     </div>
   );
 }
