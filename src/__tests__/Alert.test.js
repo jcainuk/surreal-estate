f078bbaa91d3cc1cf7fb1e120c683cf594e160bb
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-29 14:40:43 +0100

    Addressed the missing router problem in App.js and amended the Alert test

--- src/__tests__/Alert.test.js
+++ src/__tests__/Alert.test.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Alert from "../components/Alert";
 
 describe("Alert", () => {
-  test("it displays an error message", () => {
-    const { asFragment } = render(<Alert message="Error!" isSuccess={false} />);
-    expect(screen.getByText("Error!")).toBeInTheDocument();
-    expect(asFragment()).toMatchSnapshot();
+  test("displays an error message", () => {
+    const { getByText } = render(<Alert message="Error!" />);
+    expect(getByText(/Error/).textContent).toBe("Error!");
   });
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/App.css";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Properties from "./Properties";
 import AddProperty from "./AddProperty";
@@ -9,11 +9,13 @@ import Alert from "./Alert";
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={Properties} />
-        <Route exact path="/AddProperty" component={AddProperty} />
-      </Switch>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Properties} />
+          <Route exact path="/AddProperty" component={AddProperty} />
+        </Switch>
+      </Router>
     </div>
   );
 }
