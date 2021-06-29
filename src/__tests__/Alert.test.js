bd69dd988ccd3a9e76cf79b38d4fae34e64ea31d
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-29 14:57:42 +0100

    Installed proptypes, fixing alert tests and components

--- src/__tests__/Alert.test.js
+++ src/__tests__/Alert.test.js
@@ -7,4 +7,15 @@ describe("Alert", () => {
     const { getByText } = render(<Alert message="Error!" />);
     expect(getByText(/Error/).textContent).toBe("Error!");
   });
+
+  test("displays a success message", () => {
+    const { getByText } = render(<Alert message="Success!!!!" success />);
+
+    expect(getByText(/Success/).textContent).toBe("Success!!!!");
+  });
+  it("does not render if message prop is empty", () => {
+    const { asFragment } = render(<Alert message="" />);
+    const alert = asFragment();
+    expect(alert).toMatchSnapshot();
+  });
 });
