9670f99ab05c2a34dcbf4a4fd3f3567944ef1996
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-30 15:00:20 +0100

    Added a useEffect anf imoported it in Properties

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import PropertyCard from "./PropertyCard";
@@ -9,6 +9,7 @@ const Properties = () => {
   };
 
   const [properties, setProperties] = useState([]);
+  useEffect(() => {}, []);
 
   return <div>Properties Page</div>;
 };
