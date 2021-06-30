ab113661ed5a690b3f425595d311f2015b6557f3
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-30 14:50:42 +0100

    Started importing required dependencies into Properties

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import PropertyCard from "./PropertyCard";
 
 const Properties = () => <div>Properties Page</div>;
 
