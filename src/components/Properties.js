3d47c3ef43cb9d465911cc9d7d3297480a28fb9d
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-30 14:58:41 +0100

    Set initial state of Properties to an empty array.

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,8 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import PropertyCard from "./PropertyCard";
 
-const Properties = () => <div>Properties Page</div>;
+const Properties = () => {
+  const initialState = {
+    properties: [],
+  };
+
+  const [properties, setProperties] = useState([]);
+
+  return <div>Properties Page</div>;
+};
 
 export default Properties;
