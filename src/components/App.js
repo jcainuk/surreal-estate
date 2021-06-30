d4ae469fef895c1d05707e6a5d0c7d86a087f527
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-30 16:03:12 +0100

    Refactored app

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import { Switch, Route, Redirect } from "react-router-dom";
 import NavBar from "./NavBar";
 import Properties from "./Properties";
 import AddProperty from "./AddProperty";
+import SavedProperties from "./SavedProperties";
+import "./Alert";
 
-function App() {
+const App = () => {
+  const [userID, setUserID] = useState("");
+  const [savedProperties, setSavedProperties] = useState();
+  const [myProperties, setMyProperties] = useState();
+
+  const handleLogin = (response) => {
+    setUserID(response.userID);
+  };
+  useEffect(() => {
+    if (userID) {
+      axios
+        .get("http://localhost:4000/api/v1/Favourite")
+        .then((results) => {
+          setSavedProperties(results.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [userID]);
+
+  const handleLogout = () => {
+    window.FB.logout(() => {});
+    setUserID("");
+  };
+
   return (
     <div className="App">
-      <Router>
-        <NavBar />
+      <NavBar onLogin={handleLogin} userID={userID} onLogout={handleLogout} />
+      <div className="navlinks">
         <Switch>
-          <Route exact path="/" component={Properties} />
-          <Route exact path="/AddProperty" component={AddProperty} />
+          <Route exact path="/" render={() => <Properties userID={userID} />} />
+          <Route exact path="/AddProperty">
+            <AddProperty />
+          </Route>
+          <Route
+            exact
+            path="/SavedProperties"
+            render={() =>
+              userID ? (
+                <SavedProperties
+                  myProperties={myProperties}
+                  setMyProperties={setMyProperties}
+                  savedProperties={savedProperties}
+                  userID={userID}
+                />
+              ) : (
+                <Redirect to="/" />
+              )
+            }
+          />
         </Switch>
-      </Router>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
