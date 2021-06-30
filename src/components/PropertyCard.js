2a9034190f45e9af37fee17dcf830b292296593d
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-30 14:21:51 +0100

    Wrote the tests for and created the corresponding files for the PropertyCard

---
+++ src/components/PropertyCard.js
@@ -0,0 +1,3 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import "../styles/Propertycard.css";
---
+++ src/tests/components/PropertyCard.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import PropertyCard from "../components/PropertyCard";
+
+describe("Property Card", () => {
+  const validProps = {
+    title: "Beautiful Flat",
+    propertyId: "",
+    userId: "1234",
+    myProperties: [],
+    favouriteId: "",
+    type: "Flat",
+    bedrooms: 1,
+    bathrooms: 1,
+    price: 125000,
+    city: "Sheffield",
+    email: "JoeBloggs@test.com",
+    onSaveProperty: () => {},
+    userID: "1234",
+    key: "1234",
+  };
+
+  describe("Property Card renders items based on props", () => {
+    beforeEach(() => render(<PropertyCard {...validProps} />));
+
+    test("title", () => {
+      expect(screen.getByText(/Beautiful Flat/i)).toBeInTheDocument();
+    });
+    test("type", () => {
+      expect(screen.getByText(/Flat/)).toBeInTheDocument();
+    });
+    test("price", () => {
+      expect(screen.getByText(/125000/)).toBeInTheDocument();
+    });
+
+    test("city", () => {
+      expect(screen.getByText(/Sheffield/i)).toBeInTheDocument();
+    });
+  });
+});
