361472d07a41584e9fdeb085d15cc8b857c28154
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-30 15:58:54 +0100

    Refactoring NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,57 @@
 import React from "react";
-import "../styles/NavBar.css";
 import { Link } from "react-router-dom";
-import logo from "../images/logo.png";
+import "../styles/NavBar.css";
+import FacebookLogin from "react-facebook-login";
+import PropTypes from "prop-types";
+import { FaFacebookSquare } from "react-icons/fa";
 
-const NavBar = () => {
+const NavBar = ({ onLogin, userID, onLogout }) => {
   return (
     <div className="navbar">
-      <img src={logo} className="logo" alt="surreal-estate-logo" />
-      <ul className="navbar-links">
+      <img src="/images/logo.png" alt="navbar-logo" />
+      <div className="navbar-menu">
         <li>
-          <Link className="navbar-links-item" to="/">
+          <Link className="nav-links" to="/">
             View Properties
           </Link>
         </li>
         <li>
-          <Link className="navbar-links-item" to="/AddProperty">
-            Add a Property
+          <Link className="nav-links" to="/AddProperty">
+            Add Property
           </Link>
         </li>
-      </ul>
+        {userID ? (
+          <li>
+            <Link className="nav-links" to="/SavedProperties">
+              Saved Properties
+            </Link>
+          </li>
+        ) : (
+          " "
+        )}
+      </div>
+
+      {userID ? (
+        <button className="facebook" type="button" onClick={onLogout}>
+          <FaFacebookSquare />
+          Log out
+        </button>
+      ) : (
+        <FacebookLogin
+          appId="442074327121841"
+          fields="name,email,picture"
+          callback={onLogin}
+          cssClass="facebook"
+        />
+      )}
     </div>
   );
+};
+
+NavBar.propTypes = {
+  onLogin: PropTypes.func.isRequired,
+  userID: PropTypes.number.isRequired,
+  onLogout: PropTypes.func.isRequired,
 };
 
 export default NavBar;
