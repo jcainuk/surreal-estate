73a1553a4da85b8dd074fa3277c77f8b42d23433
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-06-30 15:57:55 +0100

    Refactoring Alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,19 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
 import "../styles/Alert.css";
+import PropTypes from "prop-types";
 
 const Alert = ({ message, success }) => {
+  Alert.defaultProps = {
+    success: false,
+  };
   if (!message) return null;
-
   return (
     <div className={`Alert alert-${success ? "success" : "error"}`}>
       {message}
     </div>
   );
-};
-
-Alert.defaultProps = {
-  success: false,
 };
 
 Alert.propTypes = {
